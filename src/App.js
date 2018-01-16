67149b591916798d0c414213f973586e083c5d96
Camila Daniels <camila.daniels926@gmail.com>
2018-01-16 15:51:40 -0600

    Camila Daniels camila.daniels926@gmail.com. The initialcommit.js file contains the code we went over together during lecture. I followed steps 1-11 and the webpage seems to be rendering as expected.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import './App.css';
 import Navigation from './components/Navigation';
 import { Home, About, Contact} from './components';
 
+import { Route } from 'react-router-dom';
+
+
+
 const App = () => (
   <div>
     <Navigation /> 
+
+    <Route path="/" component={Home} exact/>
+	<Route path="/about" component={About}/>
+	<Route path="/contact" component={Contact}/>
   </div>
 )
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
 
+import { Link } from 'react-router-dom';
+
 const Navigation = () => {
   return (
     <div>
       <Nav className="App">
         <h1>React Router Mini</h1>
         <NavItem>
-          <NavLink href="#">Home</NavLink>
+          <Link className="navlink" to="/">Home</Link>
         </NavItem>
         <NavItem>
-          <NavLink href="#">About</NavLink>
+          <Link className="navlink" to="/about">About</Link>
         </NavItem>
         <NavItem>
-          <NavLink href="#">Contact</NavLink>
+          <Link className="navlink" to="/contact">Contact</Link>
         </NavItem>
       </Nav>
     </div>
